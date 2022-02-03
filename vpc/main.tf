4810e571bd5fdafe5b72f39c8b8961ee31c5da8d
Robert Walczak <robert.walczak@cloudreach.com>
2022-02-03 16:25:58 +0000

    Testing

--- vpc/main.tf
+++ vpc/main.tf
@@ -2,6 +2,7 @@ resource "aws_vpc" "main" {
   cidr_block = var.vpc_cidr
   tags = {
     Region = var.aws_region
+    AccountId = var.account_id
   }
 }
 
--- vpc/variables.tf
+++ vpc/variables.tf
@@ -6,3 +6,7 @@ variable "aws_region" {
   description = "The AWS Region name"
 }
 
+variable "account_id" {
+  description = "The AWS Account ID"
+}
+
